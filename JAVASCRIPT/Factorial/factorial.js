745e464a5dfc42e07c55b0143cf84c28329c347a
olatz80 <olatz80@gmail.com>
2015-03-10 21:27:12 +0100

    factorial

---
+++ JAVASCRIPT/Factorial/factorial.js
@@ -0,0 +1,16 @@
+var numero = 5;
+var factorial = 1;
+//var i=1;
+for (var i = 1; i <= numero; i++) {
+    factorial *= i;
+}
+alert(factorial);
+
+
+/*con while
+
+while(0<numero)
+{
+factorial = factorial*numero--;
+}
+alert... }*/
